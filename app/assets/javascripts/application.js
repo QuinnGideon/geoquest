2f97eb76dac43b9224f7642b9d5a60beae8216b4
Quinn Gideon <Quinn.Gideon@gmail.com>
2016-06-23 19:54:54 -0400

    hiding answer div on load, and showing on submit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,41 +17,43 @@
 
 $(document).ready(function() {
 
-	 // $('#answers').hide();
+	 // $('.answers').hide();
 	getRandomCountry();
 
 	$("#submit").click(function(){ 
 
 		  if($("#opt0").val()=="1" && $("#optcheck0").is(":checked")){
-		  	$('#answers').text("Fantastic");
+		  	$('.answers').text("Fantastic");
 	setTimeout(function(){window.location.reload();
 		  }, 1000);
 
 		  }
 
 		  else if($("#opt1").val()=="1" && $("#optcheck1").is(":checked")){
-		  		  	$('#answers').text("You got it!");
+		  		  	$('.answers').text("You got it!");
 		  	setTimeout(function(){window.location.reload();
 }, 1000);
 		  }
 		  
 			else if($("#opt2").val()=="1" && $("#optcheck2").is(":checked")){
-							$('#answers').text("Correct!");
+							$('.answers').text("Correct!");
 				setTimeout(function(){window.location.reload();
 }, 1000);
 		  }
 		 
 			else if($("#opt3").val()=="1" && $("#optcheck3").is(":checked")){
-							$('#answers').text("You rock!");
+							$('.answers').text("You rock!");
 				setTimeout(function(){window.location.reload();
 }, 1000);
 		  }
 			else{
-				$('#answers').text("Not quite, but it's "+answer).css("color", "red");
+				$('.answers').text("Not quite, but it's "+answer).css("color", "red");
 				setTimeout(function(){window.location.reload();
 }, 3000);
 
 		}
+
+		$(".answers").show().css("left", "49.5%");
 
 	 }); 
 
